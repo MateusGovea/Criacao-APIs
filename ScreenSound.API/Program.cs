0174fdc38b35563963d8af8ee6278a205cc7cdd3
MateusGovea <matrgovea@gmail.com>
2025-03-26 11:36:11 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound.API/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using ScreenSound.Banco;
+using ScreenSound.Modelos;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+var app = builder.Build();
+
+// Pagina Inicial
+app.MapGet("/", () => "Página Carregada");
+
+// GET
+app.MapGet("/Artistas", () =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    return Results.Ok(dal.Listar());
+});
+
+// GET by Nome
+app.MapGet("/Artistas/{nome}", (string nome) =>
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (artista is null)
+    {
+        return Results.NotFound();
+    }
+
+    return Results.Ok(artista);
+});
+
+// POST
+app.MapPost("/Artistas", ([FromBody]Artista artista) => 
+{
+    var dal = new DAL<Artista>(new ScreenSoundContext());
+    dal.Adicionar(artista);
+
+    return Results.Ok();
+});
+
+app.Run();
